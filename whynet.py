ea88a1a65645c0f7e4ef9d2260cfcbf0707e864a
amiad <hatul@hatul.info>
2013-05-12 23:36:58 +0300

    update ArgumentParser description

--- whynet.py
+++ whynet.py
@@ -2,7 +2,7 @@
 import sys, urllib2, os, re, argparse
 
 # get arguments
-parser = argparse.ArgumentParser(description='Download video from Ynet (and Hot & Reshet).')
+parser = argparse.ArgumentParser(description='Download video from Ynet (and Hot).')
 parser.add_argument('url', help = 'page url')
 parser.add_argument('-o', dest = 'file', help = 'output file')
 args = parser.parse_args()
