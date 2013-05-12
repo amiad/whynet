3e480d290b12ab6c9526fada72bbf1c077098535
Amiad <hatul@hatul.info>
2013-05-12 22:17:57 +0300

    add comments

--- whynet.py
+++ whynet.py
@@ -6,16 +6,19 @@ parser = argparse.ArgumentParser(description='Download video from Ynet (and Hot 
 parser.add_argument('url', help = 'page url')
 parser.add_argument('-o', dest = 'file', help = 'output file')
 args = parser.parse_args()
-	
+
+# get page	
 url = args.url
 page = urllib2.urlopen(url).read()
 
+# find rtmp url
 link = (re.search('(rtmp%3A%2F%2F((?!%22).)*)%22',page))
 if link is None: 
 	print "Clip not found!"
 	exit()
 link = urllib2.unquote(link.group(1)) 
 
+# grab video
 if args.file is None:
 	filename = os.path.basename(link)
 else:
