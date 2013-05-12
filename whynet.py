a660bd8aaffb37cff479a415fab073b72e18b8a7
Amiad <hatul@hatul.info>
2013-05-12 15:18:55 +0300

    first script

---
+++ whynet.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python2
+import sys, urllib2, os, re
+
+url = sys.argv[1]
+page = urllib2.urlopen(url).read()
+
+link = (re.search('(rtmp%3A%2F%2F((?!%22).)*)%22',page))
+if link is None: 
+	print "Not Found Clip"
+	exit()
+link = urllib2.unquote(link.group(1)) 
+filename = os.path.basename(link)
+print ("Download: " + link)
+os.system('rtmpdump -r ' + link + " -o " + filename)
